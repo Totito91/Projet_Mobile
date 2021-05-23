156b7064a9f08fedcee216a0bf9deff800c94581
Boussiquet <theo.boussiquet@orange.Fr>
2021-05-23 14:54:08 +0200

    Cache

--- app/src/main/java/com/example/projet_mobile/presentation/Singletons.kt
+++ app/src/main/java/com/example/projet_mobile/presentation/Singletons.kt
@@ -1,14 +1,23 @@
 package com.example.projet_mobile.presentation
 
+import com.example.projet_mobile.presentation.AOEApplication.Companion.context
 import com.example.projet_mobile.presentation.api.AOEApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 class Singletons{
     companion object{
-             val aoeApi: AOEApi = Retrofit.Builder()
+        var cache = Cache(File(context?.cacheDir,"response"), 10 * 1024 *1024)
+        val okHttpClient : OkHttpClient = OkHttpClient().newBuilder()
+                .cache(cache)
+                .build()
+        val aoeApi: AOEApi = Retrofit.Builder()
                 .baseUrl("https://age-of-empires-2-api.herokuapp.com/api/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build().create(AOEApi::class.java)
     }
 }
